0d1bbc2081bfc20187a7a292bfa4678a04a227ac
coo_coo1 <kaloianbch@gmail.com>
2018-01-27 18:21:15 +0000

    Add Player and Camera

--- src/client/Assets/Scripts/cameraController.cs
+++ src/client/Assets/Scripts/cameraController.cs
@@ -1,15 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class cameraController : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class CameraController : MonoBehaviour
+{
+    //Plane limits
+    [SerializeField] int xMax;
+    [SerializeField] int xMin;
+    [SerializeField] int zMax;
+    [SerializeField] int zMin;
+    //Orthographic size limits and zoom increase
+    [SerializeField] float zoomSpeed;
+    [SerializeField] float zoomMax;
+    [SerializeField] float zoomMin;
+    public GameObject player;       //Public variable to store a reference to the player game object
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+    private void Update()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+
+        // Zoom in
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            Camera.main.orthographicSize -= zoomSpeed;
+        }
+
+        // Zoom out
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            Camera.main.orthographicSize += zoomSpeed;
+        }
+    }
+    private void LateUpdate()
+    {
+        //Limiting camera to the plane
+        Vector3 tmpPos = transform.position;
+        tmpPos.x = Mathf.Clamp(tmpPos.x, xMin + 3 * Camera.main.orthographicSize, xMax - 3 * Camera.main.orthographicSize);
+        tmpPos.z = Mathf.Clamp(tmpPos.z, zMin + Camera.main.orthographicSize, zMax - Camera.main.orthographicSize);
+        transform.position = tmpPos;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, zoomMin, zoomMax);
+        //Debug.Log("CamX:" + transform.position.x + " CamZ:" + transform.position.z);
+        Debug.Log("CamZoom:" + Camera.main.orthographicSize);
+    }
 }
---
+++ src/client/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    public float speed;
+    private bool move;
+    private Vector3 target;
+    private void Start()
+    {
+        //TODO - replace this completly
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (!move)
+            {
+                move = true;
+            }
+        }
+
+        if (move)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+    }
+}
