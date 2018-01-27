15c8d159b4115c69bc33954ba770c329526cc1f3
Alexander Clarke <alexander_m_clarke@live.co.uk>
2018-01-27 13:45:27 +0000

    Add physical grid

--- src/client/Assets/GridSpawner.cs
+++ src/client/Assets/GridSpawner.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraController : MonoBehaviour {
+public class GridSpawner : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
 }
